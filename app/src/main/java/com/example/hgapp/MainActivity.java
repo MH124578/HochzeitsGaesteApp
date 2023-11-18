f50a14631b1ab65cf5d084ab98648dacc545e59b
micha <micha@DESKTOP-595PA4O>
2023-11-18 12:49:28 +0100

    bilder speichern in datenbank geht anzeigen nicht

--- app/src/main/java/com/example/hgapp/MainActivity.java
+++ app/src/main/java/com/example/hgapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,6 +28,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public class MainActivity extends Activity {
@@ -35,6 +38,7 @@ public class MainActivity extends Activity {
     private ImageView imageView;
     private EditText editText;
     private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,19 +84,33 @@ public class MainActivity extends Activity {
                 MultipartBody.Part body = MultipartBody.Part.createFormData("file", "image.jpg", requestFile);
 
                 ApiService service = getClient().create(ApiService.class);
-                int userId = 1; // Ersetzen Sie dies durch die tatsächliche Benutzer-ID
+                int userId = 1;  // Stellen Sie sicher, dass Sie die richtige Benutzer-ID verwenden.
                 Call<ResponseBody> call = service.uploadImage(userId, body);
+
                 call.enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                         Log.d("UploadActivity", "Upload successful: " + response.toString());
-                        // Erfolgreiche Antwort vom Server
+                        if (response.isSuccessful() && response.body() != null) {
+                            try {
+                                String responseString = response.body().string();
+                                int entryId = Integer.parseInt(responseString);  // Parse die ID aus der Antwort
+                                fetchAndDisplayImage(entryId);  // Rufe die Methode auf, um das Bild anzuzeigen
+
+                                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } catch (IOException | NumberFormatException e) {
+                                Log.e("UploadActivity", "Fehler beim Parsen der Antwort", e);
+                            }
+                        }
                     }
+
 
                     @Override
                     public void onFailure(Call<ResponseBody> call, Throwable t) {
                         Log.e("UploadActivity", "Upload failed", t);
-                        // Fehler beim Hochladen
                     }
                 });
             }
@@ -103,7 +121,7 @@ public class MainActivity extends Activity {
 
     private Retrofit getClient() {
         return new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8000/") // Ersetzen Sie dies durch Ihre Server-URL
+                .baseUrl("http://10.0.2.2:8000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
@@ -130,7 +148,7 @@ public class MainActivity extends Activity {
 
         @Override
         public void writeTo(BufferedSink sink) throws IOException {
-            byte[] buffer = new byte[2048]; // Puffergröße
+            byte[] buffer = new byte[2048];
             int read;
             try {
                 while ((read = inputStream.read(buffer)) != -1) {
@@ -142,5 +160,34 @@ public class MainActivity extends Activity {
                 }
             }
         }
+    }
+
+    interface YourApiService {
+        @GET("/images/{entryId}")
+        Call<ResponseBody> getImage(@Path("entryId") int entryId);
+    }
+
+    private void fetchAndDisplayImage(int entryId) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        YourApiService service = retrofit.create(YourApiService.class);
+        Call<ResponseBody> call = service.getImage(entryId);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                    runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            }
+        });
     }
 }
